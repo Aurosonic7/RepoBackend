bbe2b5eba143b8811e2f06f12c392af06eb21221
Aurosonic7 <99984328+Aurosonic7@users.noreply.github.com>
2025-04-21 22:54:38 -0600

    add(feat): app_repobackend, mysql and mongo docker

--- config/config.js
+++ config/config.js
@@ -1,14 +1,14 @@
-import 'dotenv/config';
-import development from './env/development.js';
-import production from './env/production.js';
+import "dotenv/config";
+import development from "./env/development.js";
+import production from "./env/production.js";
 
 const envConfig =
-  process.env.NODE_ENV === 'production' ? production : development;
+  process.env.NODE_ENV === "production" ? production : development;
 
 const config = {
   app: {
     port: Number(process.env.PORT) || 3000,
-    jwtSecret: process.env.JWT_SECRET || 'change_this_secret'
+    jwtSecret: process.env.JWT_SECRET || "change_this_secret",
   },
   mysql: envConfig.MYSQL,
   mongo: envConfig.MONGO,
--- config/env/production.js
+++ config/env/production.js
@@ -1,12 +1,12 @@
 export default {
-    MYSQL: {
-        HOST: process.env.MYSQL_HOST,
-        USER:  process.env.MYSQL_USER,
-        PASSWORD: process.env.MYSQL_PASSWORD,
-        DATABASE: process.env.MYSQL_DATABASE,
-        PORT: Number(process.env.MYSQL_PORT),
-    },
-    MONGO: {
-        URI: process.env.MONGO_URI,
-    }
+  MYSQL: {
+    HOST: process.env.MYSQL_HOST,
+    USER: process.env.MYSQL_USER,
+    PASSWORD: process.env.MYSQL_PASSWORD,
+    DATABASE: process.env.MYSQL_DATABASE,
+    PORT: Number(process.env.MYSQL_PORT),
+  },
+  MONGO: {
+    URI: process.env.MONGO_URI,
+  },
 };
