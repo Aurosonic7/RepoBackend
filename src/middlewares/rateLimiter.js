954ffa42e51d618fd4fb995b485f642cb467664b
Aurosonic7 <99984328+Aurosonic7@users.noreply.github.com>
2025-05-14 20:21:06 -0600

    feat(add): max rate-limit

--- src/middlewares/rateLimiter.js
+++ src/middlewares/rateLimiter.js
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 export const rateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutos
-  max: 1000, // Máximo 100 peticiones por IP en el periodo
+  max: 100000, // Máximo 100 peticiones por IP en el periodo
   standardHeaders: true, // Devuelve información de límite en cabeceras RateLimit-*
   legacyHeaders: false, // Desactiva cabeceras X-RateLimit-*
   message: {
