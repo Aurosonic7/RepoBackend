acd89d62500a57d76de9763a5ac9801ffc883c8a
Aurosonic7 <99984328+Aurosonic7@users.noreply.github.com>
2025-06-01 21:36:58 -0600

    feat(update): add patch in cors.js

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,27 +1,27 @@
 import cors from "cors";
 
 const allowedOrigins = [
-  "https://repo-web.onrender.com",  // Tu frontend en producción
+  "https://repo-web.onrender.com", // Tu frontend en producción
   "http://localhost:5173",
-  "http://127.0.0.1:5173"  // Añadir IPv4 local
+  "http://127.0.0.1:5173", // Añadir IPv4 local
 ];
 
 const corsOptions = {
   origin: function (origin, callback) {
     // Permitir solicitudes sin origen (como aplicaciones móviles o Postman)
     if (!origin) return callback(null, true);
-    
+
     if (allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
       console.log("Origen bloqueado por CORS:", origin);
-      callback(new Error('No permitido por CORS'));
+      callback(new Error("No permitido por CORS"));
     }
   },
   credentials: true,
   optionsSuccessStatus: 200,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
 
 export const corsMiddleware = cors(corsOptions);
